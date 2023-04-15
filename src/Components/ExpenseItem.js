fba1dfe3d0ebd5daa839f4042fadff38b6ec376d
Abhishek Chhawari <abhishekch360@gmail.com>
2023-04-15 13:28:00 +0530

    custom tags and props

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,16 +1,15 @@
 import './ExpenseItem.css';
-
+import ExpenseDate from './ExpenseDate'; 
+import Card from './Card';
 function ExpenseItem(props) {
-
-   
     return (
-        <div className='expense-item-body'>
-            <div>{props.date.toISOString()}</div>
-            <div className='expense-item-detail'>
+        <Card className='expense-item '>
+            <ExpenseDate date = {props.date}/>
+            <div className='expense-item__description'>
                 <h2>{props.title}</h2>
-                <div className='expense-item-price'>${props.amount}</div>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
-        </div>
+        </Card>
     );
 }
 
